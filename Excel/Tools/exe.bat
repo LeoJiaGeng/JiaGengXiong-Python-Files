7b39cb83211ad3d795c4a9b68870109fb69652db
xiongjiageng <jgxiong@whu.edu.cn>
2023-10-21 12:43:37 +0800

    时间：2023.10.21 内容：更改bat脚本，增加release的压缩包，默认配置rar的压缩环境

--- Excel/Tools/exe.bat
+++ Excel/Tools/exe.bat
@@ -4,7 +4,8 @@ mode con cols=50 lines=50
 :: 需要编译的文件和执行编译的软件位置
 set MAIN_PY_NAME=app.py
 set ICO_NAME=MS.ico
-set SOFT_PATHS="D:\Document\Python_Files\Python_virtual\Scripts"
+set SOFT_PATHS="E:\Pyqt5_Python\Scripts"
+set SOFT_VERSION=v1.0.0.rar
 
 :: 设置文件夹
 set PROJECT_NAME=%~dp0..
@@ -13,6 +14,7 @@ set PY_VERSION="%PY_WORKPATH%\%MAIN_PY_NAME%"
 
 set ICO_VERSION="%PY_WORKPATH%\Resource\pictures\icos\%ICO_NAME%"
 set RELEASE_PATHS="%PROJECT_NAME%\release"
+set RELEASE_VERSION_PATHS="%PROJECT_NAME%\release\dist"
 
 :: 查看是否存在文件夹
 cd %PY_WORKPATH%
@@ -27,11 +29,26 @@ cd %RELEASE_PATHS%
 copy %ICO_VERSION% MS.ico
 call pyinstaller.exe -Dw --icon=MS.ico %PY_VERSION%
 
-if %ERRORLEVEL% == 1 goto Failure
-echo exe file has been converted 
+if %ERRORLEVEL% == 1 (
+    goto Failure1
+) else (
+
+    echo exe file has been converted 
+    ::发行版本
+    cd %RELEASE_VERSION_PATHS%
+    call rar.exe a %RELEASE_PATHS%\%SOFT_VERSION% "app"
+
+    if %ERRORLEVEL% == 1 goto Failure2
+    echo rar file has been generated 
+    exit
+)
+
+:Failure1
+@echo exe file ERR
+pause
 exit
 
-:Failure
-@echo ERR
+:Failure2
+@echo rar file ERR
 pause
 exit
--- Quatum/Tools/exe.bat
+++ Quatum/Tools/exe.bat
@@ -4,12 +4,14 @@ mode con cols=50 lines=50
 :: 需要编译的文件和执行编译的软件位置
 set MAIN_PY_NAME=app.py
 set SOFT_PATHS="E:\Pyqt5_Python\Scripts"
+set SOFT_VERSION=v1.0.0.rar
 
 :: 设置文件夹
 set PROJECT_NAME=%~dp0..
 set PY_WORKPATH="%PROJECT_NAME%\Demo"
 set PY_VERSION="%PY_WORKPATH%\%MAIN_PY_NAME%"
 set RELEASE_PATHS="%PROJECT_NAME%\release"
+set RELEASE_VERSION_PATHS="%PROJECT_NAME%\release\dist"
 
 :: 查看是否存在文件夹
 cd %PY_WORKPATH%
@@ -23,11 +25,25 @@ call activate.bat
 cd %RELEASE_PATHS%
 call pyinstaller.exe -Dw %PY_VERSION%
 
-if %ERRORLEVEL% == 1 goto Failure
-echo exe file has been converted 
+if %ERRORLEVEL% == 1 (
+    goto Failure1
+) else (
+    echo exe file has been converted 
+    ::发行版本
+    cd %RELEASE_VERSION_PATHS%
+    call rar.exe a %RELEASE_PATHS%\%SOFT_VERSION% "app"
+
+    if %ERRORLEVEL% == 1 goto Failure2
+    echo rar file has been generated 
+    exit
+)
+
+:Failure1
+@echo exe file ERR
+pause
 exit
 
-:Failure
-@echo ERR
+:Failure2
+@echo rar file ERR
 pause
 exit
